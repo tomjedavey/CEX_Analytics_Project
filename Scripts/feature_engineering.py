13e8720b7c3c76f68fdbb060a46c30472efdd7ae
tomjedavey <tomjedavey@gmail.com>
2025-07-09 08:23:21 +0100

    push to make sure its updated

--- Scripts/feature_engineering.py
+++ Scripts/feature_engineering.py
@@ -4,23 +4,9 @@
 
 #Processing data to add new features for Analytic Score 1 (AS_1) - Revenue Contribution Score (RCS)
 
+
 import sys
 import os
-import pandas as pd
+sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__), '..')))
 
-# Add the Source_Code_Package to the Python path
-sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__), '..', 'Source_Code_Package', 'features')))
-
-from source_code_package import engineer_features_1
-
-# Read the raw data
-raw_data = pd.read_csv(os.path.join('..', 'data', 'raw_data', 'initial_raw_data_polygon.csv'))
-
-# Engineer features
-processed_data = engineer_features_1(raw_data)
-
-# Ensure the processed_data directory exists
-os.makedirs(os.path.join('..', 'data', 'processed_data'), exist_ok=True)
-
-# Save the processed data to CSV
-processed_data.to_csv(os.path.join('..', 'data', 'processed_data', 'new_features_data.csv'), index=False)
+from source_code_package.features.AS_1_features import my_function
