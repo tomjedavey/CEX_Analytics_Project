cdb7edd5f8ec22981beacfa19dd0703ee4127ded
tomjedavey <tomjedavey@gmail.com>
2025-09-11 19:05:32 +0100

    pushing to restart vscode

--- scripts/analytic_scores_merging_execution.py
+++ scripts/analytic_scores_merging_execution.py
@@ -2,10 +2,10 @@ import sys
 import os
 
 # Ensure the utils package is importable
-sys.path.append(os.path.join(os.path.dirname(__file__), '../../source_code_package/utils'))
+sys.path.append(os.path.join(os.path.dirname(__file__), '../source_code_package/utils'))
 
 from source_code_package.utils.analytic_score_merging import merge_analytic_scores
 
 if __name__ == "__main__":
     merge_analytic_scores()
-    print("Merged analytic scores CSV created at artifacts/Dashboards/merged_analytic_scores.csv")
+    print("Merged analytic scores CSV created at data/processed_data/merged_analytic_scores.csv")
